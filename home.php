744ff839ee39d78bc467be3df6d6ccac13af9d71
Ethan Grim <ethangrimmy33@gmail.com>
2023-07-10 23:09:40 -0400

    sample chart 7/10/23

--- home.php
+++ home.php
@@ -1,11 +1,37 @@
 <?php 
 include_once 'header.php'
 ?>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.4/Chart.js"></script>
 <div>
     <h1 class = "homeTitle">
         Hey <?php echo($_SESSION['userName']);?>!
     </h1>
+    <canvas id="goalChart" style="width:100%;max-width:600px"></canvas>
 </div>
+
+<script>
+const xValues = ["Italy", "France", "Spain", "USA", "Argentina"];
+const yValues = [55, 49, 44, 24, 15];
+const barColors = ["#FFEE12", "green","blue","orange","brown"];
+
+new Chart("goalChart", {
+  type: "pie",
+  data: {
+    labels: xValues,
+    datasets: [{
+      backgroundColor: barColors,
+      data: yValues
+    }]
+  },
+  options: {
+    legend: {display: true},
+    title: {
+      display: true,
+      text: "Goal Budget"
+    }
+  }
+});
+</script>
 <?php
 include_once 'footer.php'
 ?>
